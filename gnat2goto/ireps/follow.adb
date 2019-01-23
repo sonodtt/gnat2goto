d7b9407723e4b276fd47bfd4340081baa4279140
Petr Bauch <petr.bauch@DB0047.ox.diffblue.com>
2019-01-23 14:47:55 +0000

    Remove bounded types

--- gnat2goto/ireps/follow.adb
+++ gnat2goto/ireps/follow.adb
@@ -5,6 +5,11 @@ package body Follow is
       while Next /= 0 and then Kind (Next) = I_Symbol_Type loop
          Next := ST (Intern (Get_Identifier (Next))).SymType;
       end loop;
+      if Kind (Next) = I_Bounded_Signedbv_Type then
+         Next :=
+           Make_Signedbv_Type (I_Subtype => Get_Subtype (Next),
+                               Width     => Get_Width (Next));
+      end if;
       return Next;
    end Follow_Symbol_Type;
 end Follow;
